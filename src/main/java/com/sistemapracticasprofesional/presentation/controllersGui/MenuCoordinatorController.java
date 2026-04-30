55763c32abdab40815d85643e70762001d6a34b9
LaurencioLm55 <laurenciolopezm@gmail.com>
2026-04-30 00:41:42 -0600

    correcciones

--- src/main/java/com/sistemapracticasprofesional/presentation/controllersGui/MenuCoordinatorController.java
+++ src/main/java/com/sistemapracticasprofesional/presentation/controllersGui/MenuCoordinatorController.java
@@ -1,7 +1,23 @@
 package com.sistemapracticasprofesional.presentation.controllersGui;
 
-public class MenuCoordinatorController {
+import com.sistemapracticasprofesional.presentation.util.Navigation;
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 
+public class MenuCoordinatorController {
     
-    
+    @FXML
+    private void handleRegisterCourse(ActionEvent event) {
+        try {
+            Navigation.changeScene(
+                    event,
+                    "GuiRegistrerCourses.fxml",
+                    "Registrar experiencia educativa"
+            );
+        } catch (IOException e) {
+           
+        }
+    }
 }
+
