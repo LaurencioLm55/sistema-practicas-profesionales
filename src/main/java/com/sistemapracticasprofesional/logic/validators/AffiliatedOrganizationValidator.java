29640f2b13cb998c5f2572c52c0eaffce8418347
leo221313 <leonardopochotl@gmail.com>
2026-05-08 17:44:16 -0600

    Correccion de validadores

--- src/main/java/com/sistemapracticasprofesional/logic/validators/AffiliatedOrganizationValidator.java
+++ src/main/java/com/sistemapracticasprofesional/logic/validators/AffiliatedOrganizationValidator.java
@@ -1,10 +1,19 @@
 package com.sistemapracticasprofesional.logic.validators;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import com.sistemapracticasprofesional.logic.dto.AffiliatedOrganizationDto;
+import com.sistemapracticasprofesional.presentation.controllersGui.UserLoginController;
 
 public class AffiliatedOrganizationValidator {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserLoginController.class);
+
     private AffiliatedOrganizationDto affiliatedOrganizationDto;
+
+    public AffiliatedOrganizationValidator(){
+
+    }
 
     public AffiliatedOrganizationValidator(
         AffiliatedOrganizationDto affiliatedOrganizationDto){
@@ -15,87 +24,80 @@ public class AffiliatedOrganizationValidator {
 
     public boolean isAffiliatedOrganizationValid () {
 
-        if(isAddressAffiliatedOrganizationValid(affiliatedOrganizationDto.getAddress())){
-
-            if (isEmailAffiliatedOrganizationValid(affiliatedOrganizationDto.getEmail())){
-
-                if (isPhoneNumberAffiliatedOrganizationValid(affiliatedOrganizationDto.getPhoneNumeber())) {
-
-                    if (isTextAffiliatedOrganizationValid(affiliatedOrganizationDto.getName()) && 
-                        isTextAffiliatedOrganizationValid(affiliatedOrganizationDto.getCity()) &&
-                        isTextAffiliatedOrganizationValid(affiliatedOrganizationDto.getState())){
-
-                            return true;
-                            
-                        }
-                }
-            }
-        }
-
         return false;
     }
 
     public boolean isTextAffiliatedOrganizationValid(String text){
 
+        boolean result = false;
+
         if ( text != null && !text.isBlank()){
 
-            if ( text.matches("[a-zA-Z0-9]+")){
+            if ( text.matches("[-a-zA-ZáéíóúÁÉÍÓÚñÑ0-9#.,\\ ]+") ){
                 
-                return true;
+                result = true;
 
             }
 
         }
 
-        return false;
+        return result;
         
     }
 
-    public boolean isAddressAffiliatedOrganizationValid(String address){
-        
-        if ( address != null && !address.isBlank()){
+    public boolean isAddressAffiliatedOrganizationValid( String address ){
 
-            if ( address.matches("[a-zA-Z0-9#]+")){
+        boolean result = false;
+        
+        if ( address != null && !address.isBlank() ){
+             
+
+            if ( address.matches("[-a-zA-ZáéíóúÁÉÍÓÚñÑ0-9#.,\\ ]+")){
                 
-                return true;
+                result = true;
 
             }
 
         }
 
-        return false;
+        return result;
 
     }
 
     public boolean isPhoneNumberAffiliatedOrganizationValid( String phoneNumber ){
 
+        boolean result = false;
+
         if ( phoneNumber != null && !phoneNumber.isBlank()){
 
             if ( phoneNumber.matches("\\d{7,15}")) {
                 
-                return true;
+                result = true;
 
             }
 
         }
 
-        return false;
+        return result;
 
     }
 
     public boolean isEmailAffiliatedOrganizationValid( String email ){
 
+        boolean result = false;
+
         if ( email != null && !email.isBlank()){
 
-            if ( email.matches("^[a-zA-Z0-9._%+\\\\-]+@[a-zA-Z0-9.\\\\-]+\\\\.[a-zA-Z]{2,}$")){
+            if ( email.matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")
+            || email.matches("^NA$")){
 
-                return true;
+                result = true;
 
             }
 
         }
 
-        return false;
+        return result;
 
     }
 
---
+++ src/main/java/com/sistemapracticasprofesional/logic/validators/CourseValidator.java
@@ -0,0 +1,61 @@
+package com.sistemapracticasprofesional.logic.validators;
+
+public class CourseValidator {
+
+    public CourseValidator(){
+
+    }
+
+    public boolean isNRCValid(String nrc){
+        
+        boolean result = false;
+
+        if ( nrc != null && !nrc.isBlank() ){
+            
+            if ( nrc.matches("//d{5}") ){
+
+            }
+
+        }
+
+        return result;
+
+    }
+
+    public boolean isPeriodValid(String period){
+
+        boolean result = false;
+
+        if ( period != null && !period.isBlank() ){
+            
+            if ( period.matches("^[//d{2}]-[//d{2}]-[//d{2}]$") ){
+
+                result = true;
+            }
+
+        }
+
+        return result;
+
+    }
+
+    public boolean isSectionValid(String section){
+
+        boolean result = false;
+
+        if ( section != null && !section.isBlank() ){
+            
+            if ( section.matches("") ){
+
+                result = true;
+            }
+
+        }
+
+        return result;
+
+    }
+
+    
+
+}
