a7f18c9fef1cba6e1f0666864e2716a4a521d89f
leo221313 <leonardopochotl@gmail.com>
2026-05-11 19:46:28 -0600

    Validador de interno

--- src/main/java/com/sistemapracticasprofesional/logic/validators/InternValidator.java
+++ src/main/java/com/sistemapracticasprofesional/logic/validators/InternValidator.java
@@ -2,4 +2,30 @@ package com.sistemapracticasprofesional.logic.validators;
 
 public class InternValidator {
     
+    public InternValidator(){
+
+    }
+
+    public boolean isStudentIdValid(){
+        boolean result = false;
+        return result;
+    }
+
+    public boolean isAgeValid(){
+        boolean result = false;
+        return result;
+    }
+
+    public boolean isInternNameValid(){
+        boolean result = false;
+        return result;
+    }
+
+    public boolean isIndigenousLanguageValid(){
+        boolean result = false;
+        return result;
+    }
+
+
+    
 }
