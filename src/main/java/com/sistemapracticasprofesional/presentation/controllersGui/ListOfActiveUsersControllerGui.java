26f511e6041af89e6501bff6473c80c7b792ff45
leo221313 <leonardopochotl@gmail.com>
2026-05-14 14:11:50 -0600

    Cambios de nombre

---
+++ src/main/java/com/sistemapracticasprofesional/presentation/controllersGui/ListOfActiveUsersControllerGui.java
@@ -0,0 +1,14 @@
+package com.sistemapracticasprofesional.presentation.controllersGui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+
+public class ListOfActiveUsersControllerGui {
+
+    @FXML
+    private ListView<String> listUser;
+
+    
+
+}
